dfced53b9e14eba2ccc4d22e0ca0ca035db8ad17
pol1klop1k <garik.b.04@gmail.com>
2024-05-27 15:48:21 +0300

    modified user_serializer

--- ToTalk/users/chatsserializers.py
+++ ToTalk/users/chatsserializers.py
@@ -11,10 +11,10 @@ class UserIdentifySerializer(serializers.ModelSerializer):
         ret["user_decency"] = DecencySerializer(instance.user_decency).data
         return ret
 
-    user_rooms = RoomSerializer(many=True)
+    user_rooms = RoomSerializer(many=True, read_only=True)
 
     class Meta:
         model = UserModel
-        fields = ["email", "last_login", "is_superuser", "is_staff", "is_active", "date_joined", "groups", "user_permissions", "user_rooms"]
+        fields = ["username", "password", "email", "last_login", "is_superuser", "is_staff", "is_active", "date_joined", "groups", "user_permissions", "user_rooms"]
         extra_kwargs = {'password': {'write_only': True}, 'email': {'required': True}}
         read_only_fields = ["last_login", "is_superuser", "is_staff", "is_active", "date_joined", "groups", "user_permissions"]
